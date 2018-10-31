c2249cc6f8f4cd64f4b10c8806e92a134a074605
Young <895254752@qq.com>
2018-10-31 10:59:56 +0800

    小额、大额审批（除了凭证的URL缺乏，其余通过测试），删去了个人中心-基本信息-安全服务

--- src/pages/smalltargetcheck.vue
+++ src/pages/smalltargetcheck.vue
@@ -53,7 +53,7 @@
                 <el-form-item label="项目编号"><span>{{ tableData[tempindex].projectid}}</span></el-form-item>
                 <el-form-item label="项目名称">{{ tableData[tempindex].name }}</el-form-item>
                 <el-form-item label="开始时间">{{ tableData[tempindex].startTime }}</el-form-item>
-                <el-form-item label="借款期数">{{ tableData[tempindex].endTime }}</el-form-item>
+                <el-form-item label="结束时间">{{ tableData[tempindex].endTime }}</el-form-item>
               </el-form>
               <el-button style="margin-top: 12px;margin-left:35%;"type="primary" size="mini" v-show="control.part2" @click="last(control)">上一步</el-button>
               <el-button style="margin-top: 12px;margin-left:5%;" type="primary" size="mini"v-show="control.part2" @click="next(control)">下一步</el-button>
@@ -75,7 +75,7 @@
                          style="margin-left:30%">
                 <el-form-item label="拆借金额"><span>{{ tableData[tempindex].money}}</span></el-form-item>
                 <el-form-item label="还款利率">{{ tableData[tempindex].rate }}</el-form-item>
-                <el-form-item label="还款日期">{{ tableData[tempindex].repayDate }}</el-form-item>
+                <el-form-item label="借款期数">{{ tableData[tempindex].repayDate }}</el-form-item>
                 <el-form-item label="还款方式">{{ tableData[tempindex].returntype }}</el-form-item>
               </el-form>
               <el-button style="margin-top: 12px;margin-left:45%;"type="primary" size="mini" v-show="control.part4"@click="last(control)">上一步</el-button>
@@ -137,39 +137,40 @@
             let returnData = [];
             let tmp;
             for(let i of res){
-              switch (i.checkState) {
-                case "ONGING": tmp = "进行中";break;
-                case "PASS": tmp = "已通过";break;
-                case "REJECT": tmp = "已被拒绝";break;
-                case "UPDATE": tmp = "用户再次提交信息";break;
-              };
-              i.checkState = tmp;
 
-              switch (i.classify) {
-                case "EXCHANGE_PROJECT": tmp = "交换生";break;
-                case "GMAT": tmp = "GMAT";break;
-                case "TOEFL": tmp = "TOEFL";break;
-                case "IELTS": tmp = "IELTS";break;
-                case "DAILY_EXPENSE": tmp = "日常费用";break;
-                case "CONCERT": tmp = "演唱会看比赛看剧音乐会等";break;
-                case "GAME_MOVIE_MUSIC": tmp = "游戏娱乐电影音乐";break;
-                case "TRAVEL": tmp = "旅游";break;
-                case "ELECTRONIC_DEVICE": tmp = "购买电子设备";break;
-                case "OTHER": tmp = "其他购买项比如化妆品衣服鞋";break;
-                case "CERTIFICATE_TEST": tmp = "出国所需考试的相关成绩单";break;
-                case "TRAIN": tmp = "各类考证";break;
-                case "PROFESSIONAL_QUALIFICATION": tmp = "职业资格证";break;
-              };
-              i.classify = tmp;
-
-              switch (i.returntype) {
-                case "EQUAL_PRINCIPAL": tmp = '等额本金'; break;
-                case "EQUAL_INSTALLMENT_OF_PRINCIPAL_AND_INTEREST": tmp = '等额本息'; break;
-                case "ONE_TIME_PAYMENT": tmp = '一次性还付本息'; break;
-                case "PRE_INTEREST": tmp = '先息后本'; break;
-              }
-
-              i.returntype = tmp;
+              // switch (i.checkState) {
+              //   case "ONGING": tmp = "进行中";break;
+              //   case "PASS": tmp = "已通过";break;
+              //   case "REJECT": tmp = "已被拒绝";break;
+              //   case "UPDATE": tmp = "用户再次提交信息";break;
+              // };
+              // i.checkState = tmp;
+              //
+              // switch (i.classify) {
+              //   case "EXCHANGE_PROJECT": tmp = "交换生";break;
+              //   case "GMAT": tmp = "GMAT";break;
+              //   case "TOEFL": tmp = "TOEFL";break;
+              //   case "IELTS": tmp = "IELTS";break;
+              //   case "DAILY_EXPENSE": tmp = "日常费用";break;
+              //   case "CONCERT": tmp = "演唱会看比赛看剧音乐会等";break;
+              //   case "GAME_MOVIE_MUSIC": tmp = "游戏娱乐电影音乐";break;
+              //   case "TRAVEL": tmp = "旅游";break;
+              //   case "ELECTRONIC_DEVICE": tmp = "购买电子设备";break;
+              //   case "OTHER": tmp = "其他购买项比如化妆品衣服鞋";break;
+              //   case "CERTIFICATE_TEST": tmp = "出国所需考试的相关成绩单";break;
+              //   case "TRAIN": tmp = "各类考证";break;
+              //   case "PROFESSIONAL_QUALIFICATION": tmp = "职业资格证";break;
+              // };
+              // i.classify = tmp;
+              //
+              // switch (i.returntype) {
+              //   case "EQUAL_PRINCIPAL": tmp = '等额本金'; break;
+              //   case "EQUAL_INSTALLMENT_OF_PRINCIPAL_AND_INTEREST": tmp = '等额本息'; break;
+              //   case "ONE_TIME_PAYMENT": tmp = '一次性还付本息'; break;
+              //   case "PRE_INTEREST": tmp = '先息后本'; break;
+              // }
+              //
+              // i.returntype = tmp;
 
 
 
@@ -183,14 +184,14 @@
                 userstate: i.checkState,//checkState?非结构化信息审核情况
                 projectid: i.targetId,//项目编号
                 startTime: i.startTime,//项目名称
-                endTime: i.duration,//结束时间
+                endTime: i.endTime,//结束时间
                 classify:i.classify,//资金用途分类
                 desc: i.explanation,//资金用途详述
                 money: i.money,//拆借金额
-                rate: '10%',//还款利率
-                repayDate:'2018-9-25',//还款日期
+                rate: i.creditRating,//还款利率
+                repayDate: i.duration,//还款日期
                 returntype: i.returntype,//还款方式
-                pics: '../../static/pic/小额1.png',//凭证路径
+                pics: i.proof,//凭证路径
               });
             }
             console.log("checkState:"+returnData[0].checkState);
@@ -268,19 +269,19 @@
 
       },
       send1: function (targetId) {
+        let flag = false;
         console.log(targetId+"号核审状态修改，准备发送");
         //这里会将状态改变传给后端，重新加载页面的时候状态就会传过来，自动跳转到状态2的情况
-        this.$axios.post('adminTarget/targetcheck', {
-          targetId: targetId,
-          result: "PASS"
+        this.$axios.get('adminTarget/targetcheck', {
+          params:{
+            targetId: targetId,
+            result: "PASS"
+          }
         })
           .then(function (response) {
             console.log(response);
             alert("修改成功！");
-            this.$message({
-              message:'提交成功',
-              type:'success',
-            });
+            flag = true;
             //self.getUserDetails();
           })
           .catch(function (response) {
@@ -288,7 +289,12 @@
             alert("修改失败！请检测您的网络连接");
           })
 
-
+        if(flag){
+          this.$message({
+            message:'提交成功',
+            type:'success',
+          });
+        }
         this.tableData.splice(this.tempindex, 1);
         this.active = 1;
         this.control.part1=true;
